ee75c60544370b4feeb037b9c4036f156b301128
Dmitry <dimonn12@hotmail.com>
2015-12-05 01:12:40 +0300

    Password changing messages

--- src/main/webapp/scripts/app/account/password/password.controller.js
+++ src/main/webapp/scripts/app/account/password/password.controller.js
@@ -2,24 +2,55 @@
 
 angular.module('jHipsterApplicationApp')
     .controller('PasswordController', function ($scope, Auth, Principal) {
+
+        $scope.responseResults = {
+            serverError: false,
+
+            success: false,
+
+            error: false,
+            doNotMatch: false,
+            tooWeak: false,
+
+            clearAll: function () {
+                $scope.responseResults.serverError = false;
+
+                $scope.responseResults.success = false;
+
+                $scope.responseResults.error = false;
+                $scope.responseResults.doNotMatch = false;
+                $scope.responseResults.tooWeak = false;
+            }
+        };
+
         Principal.identity().then(function (account) {
+            $scope.responseResults.clearAll();
             $scope.account = account;
+        }).catch(function (errorObject) {
+            $scope.responseResults.serverError = true;
         });
 
-        $scope.success = null;
-        $scope.error = null;
-        $scope.doNotMatch = null;
         $scope.changePassword = function () {
+            $scope.responseResults.clearAll();
             if ($scope.password !== $scope.confirmPassword) {
-                $scope.doNotMatch = 'ERROR';
+                $scope.responseResults.doNotMatch = true;
             } else {
-                $scope.doNotMatch = null;
                 Auth.changePassword($scope.password).then(function () {
-                    $scope.error = null;
-                    $scope.success = 'OK';
-                }).catch(function () {
-                    $scope.success = null;
-                    $scope.error = 'ERROR';
+                    $scope.responseResults.success = true;
+                }, function (errorObject) {
+                    for (var index = 0; index < errorObject.data.errorStatuses.length; index++) {
+                        var errorStatus = errorObject.data.errorStatuses[index];
+                        if (errorStatus.code === 4000003) {
+                            $scope.responseResults.tooWeak = true;
+                        }
+                        if (errorStatus.code === 5000000) {
+                            $scope.responseResults.serverError = true;
+                        }
+                    }
+
+                    if (!$scope.responseResults.tooWeak && !$scope.responseResults.serverError) {
+                        $scope.responseResults.error = true;
+                    }
                 });
             }
         };
--- src/main/webapp/scripts/app/account/settings/settings.controller.js
+++ src/main/webapp/scripts/app/account/settings/settings.controller.js
@@ -26,6 +26,9 @@ angular.module('jHipsterApplicationApp')
         Principal.identity(true).then(function (account) {
             $scope.responseResults.clearAll();
             $scope.settingsAccount = account;
+            if (null === account) {
+                $scope.responseResults.serverError = true;
+            }
         }).catch(function (errorObject) {
             $scope.responseResults.serverError = true;
         });
@@ -36,20 +39,23 @@ angular.module('jHipsterApplicationApp')
                 $scope.responseResults.success = true;
                 Principal.identity().then(function (account) {
                     $scope.settingsAccount = account;
-                }).catch(function (errorObject) {
-                    $scope.responseResults.serverError = true;
-                });
-                ;
-                Language.getCurrent().then(function (current) {
-                    if ($scope.settingsAccount.langKey !== current) {
-                        $translate.use($scope.settingsAccount.langKey);
+                    if (null === account) {
+                        $scope.responseResults.serverError = true;
                     }
                 }).catch(function (errorObject) {
                     $scope.responseResults.serverError = true;
                 });
-                ;
+                Language.getCurrent().then(function (current) {
+                    if ($scope.settingsAccount.langKey !== current) {
+                        $translate.use($scope.settingsAccount.langKey);
+                    }
+                    if (null === current) {
+                        $scope.responseResults.serverError = true;
+                    }
+                }).catch(function (errorObject) {
+                    $scope.responseResults.serverError = true;
+                });
             }).catch(function (errorObject) {
-
                 for (var index = 0; index < errorObject.data.errorStatuses.length; index++) {
                     var errorStatus = errorObject.data.errorStatuses[index];
                     if (errorStatus.code === 4000001) {
@@ -61,6 +67,10 @@ angular.module('jHipsterApplicationApp')
                     if (errorStatus.code === 5000000) {
                         $scope.responseResults.serverError = true;
                     }
+                }
+
+                if (!$scope.responseResults.errorEmailExists && !$scope.responseResults.validationError) {
+                    $scope.responseResults.serverError = true;
                 }
             });
         };
